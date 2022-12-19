1c6b38fbb1caaa199c602fbdefe891d94e963f0d
dmytrodemiancuk <moqitee@gmail.com>
2022-12-19 22:41:05 +0100

    update

--- cmd/main.go
+++ cmd/main.go
@@ -2,42 +2,88 @@ package main
 
 import (
 	"fmt"
+	"github.com/dmytrodemianchuk/crud-music/internal/repository/psql"
+	"github.com/dmytrodemianchuk/crud-music/internal/service"
+	"log"
+	"net/http"
+	"os"
+
+	grpc_client "github.com/dmytrodmeianchuk/audit-log"
 
 	"github.com/sirupsen/logrus"
 
-	"github.com/dmytrodemianchuk/crud-music/internal/repository"
-	"github.com/dmytrodemianchuk/crud-music/internal/service"
-	"github.com/dmytrodemianchuk/crud-music/internal/transport/rest"
 	"github.com/dmytrodemianchuk/crud-music/pkg/config"
 	"github.com/dmytrodemianchuk/crud-music/pkg/database"
-
-	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	srv := gin.New()
+const (
+	CONFIG_DIR  = "configs"
+	CONFIG_FILE = "main"
+)
 
-	cfg, err := config.Parse()
+func init() {
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	log.SetOutput(os.Stdout)
+	logrus.SetLevel(logrus.InfoLevel)
+}
+
+func main() {
+	cfg, err := config.New(CONFIG_DIR, CONFIG_FILE)
 	if err != nil {
-		logrus.Fatalf("error psring config: %s", err.Error())
+		log.Fatal(err)
 	}
 
+	//srv := gin.New()
+	//
+	//cfg, err := config.Parse()
+	//if err != nil {
+	//	logrus.Fatalf("error psring config: %s", err.Error())
+	//}
+
+	//init db
 	db, err := database.CreateConn(cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPass, cfg.DBName, cfg.SSLMode)
 	if err != nil {
 		logrus.Fatalf("failed to connection db: %s", err.Error())
 	}
 
-	musicRepository := repository.NewMusic(db)
-	musicService := service.NewMusic(musicRepository)
-	musicTransport := rest.NewMusic(musicService)
+	//init deps
+	hasher := hash.NewSHA1Hasher("salt")
 
-	srv.GET("/musics", musicTransport.List)
-	srv.GET("/music/:id", musicTransport.Get)
-	srv.POST("/music", musicTransport.Create)
-	srv.PUT("/music/:id", musicTransport.Update)
-	srv.DELETE("/music/:id", musicTransport.Delete)
+	musicRepository := psql.NewMusic(db)
+	musicsService := service.NewMusic(musicRepository)
 
-	if err := srv.Run(fmt.Sprintf(":%s", cfg.Port)); err != nil {
-		logrus.Fatalf("error occured while running http server %s", err.Error())
+	usersRepo := psql.NewUsers(db)
+	tokensRepo := psql.Newtokens(db)
+
+	auditClient, err := grpc_client.NewClient(9000)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	usersService := service.NewUsers(usersRepo, tokensRepo, auditClient, hasher, []byte("sample secret"))
+
+	handler := rest.NewHandler(musicsService usersService)
+
+	// init & run server
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", cfg.Server.Port),
+		Handler: handler.InitRouter(),
+	}
+
+	logrus.Info("SERVER STARTED")
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+	//musicTransport := rest.NewMusic(musicService)
+	//
+	//srv.GET("/musics", musicTransport.List)
+	//srv.GET("/music/:id", musicTransport.Get)
+	//srv.POST("/music", musicTransport.Create)
+	//srv.PUT("/music/:id", musicTransport.Update)
+	//srv.DELETE("/music/:id", musicTransport.Delete)
+	//
+	//if err := srv.Run(fmt.Sprintf(":%s", cfg.Port)); err != nil {
+	//	logrus.Fatalf("error occured while running http server %s", err.Error())
+	//}
 }
